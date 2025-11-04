43bc673401ce43ad2205390d3a9b6f6a4c2dc91d
Marcial Bernal <marcialb9328@gmail.com>
2025-11-03 20:06:48 -0700

    Finalizado web scraper

--- frontend/services/web_scraper.py
+++ frontend/services/web_scraper.py
@@ -1,17 +1,15 @@
 import pandas as pd
 import requests
-import re
+from datetime import datetime
 import os
-import time
 from bs4 import BeautifulSoup
 
-
+###
 def get_item_info(url):
     headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0 Safari/537.36",
     "Accept-Language": "es-ES,es;q=0.9",
     }
-    
     response = requests.get(url, headers=headers)
     scraper = BeautifulSoup(response.text, features='lxml')
     
@@ -44,12 +42,59 @@ def get_item_info(url):
         is_full = full_icon is not None
     except:
         is_full = False
+
+    return title, img_url, price, is_full
+
+###
+def save_to_excel(data):
+    df = pd.DataFrame(data)
+    os.makedirs("temp", exist_ok=True)
+    file_name = os.path.join("temp", "search.xlsx")
+    df.to_excel(file_name, index=False)
+    
+    return file_name
+        
+###       
+def get_search_results(query):
+    headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0 Safari/537.36",
+    "Accept-Language": "es-ES,es;q=0.9",
+    }    
+    
+    url = f"https://listado.mercadolibre.com.mx/{query}"
+    response = requests.get(url, headers=headers)
+    scraper = BeautifulSoup(response.text, features='lxml')
+    
+    products_links = []
+    for link in scraper.find_all("a", class_="poly-component__title", href=True):
+        products_links.append(link["href"])
+        
+    return products_links
     
     
-    return title, img_url, price
+###        
+def run_scraper(search_query):
+    product_urls = get_search_results(search_query)
+    all_data = []
 
-## TEST ##
-url = "https://www.mercadolibre.com.mx/teclado-mecanico-para-juegos-de-104-teclas-interruptor-azul/p/MLM61442320?pdp_filters=item_id:MLM4303320246#is_advertising=true&searchVariation=MLM61442320&backend_model=search-backend&position=1&search_layout=stack&type=pad&tracking_id=97b7bdd9-e97a-4020-8c4f-4190fda6b0ef&ad_domain=VQCATCORE_LST&ad_position=1&ad_click_id=OTM3NWRkOTMtZjNlOS00NDUyLTlmMWItMjMyMzlhNWJhNThk"
+    if product_urls:
+        for url in product_urls[:10]:
+            title, img_url, price, is_full = get_item_info(url)
+            if title != 'Title not Found':
+                all_data.append({
+                    "Date": datetime.now().strftime('%Y-%m-%d'),
+                    "Title": title,
+                    "Price": price,
+                    "Image URL": img_url,
+                    "Item URL": url,
+                    "Full": is_full
+                })
 
-title, img_url, price, is_full = get_item_info(url)
-print(title, img_url, price, is_full)
+    if all_data:
+        df = pd.DataFrame(all_data)
+        file_name = save_to_excel(all_data)
+    else:
+        df = pd.DataFrame()
+        file_name = None
+
+    return df, file_name
